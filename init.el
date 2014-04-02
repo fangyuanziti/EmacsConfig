b0a4388b64929a1e24cdbadf22da97e8878e7c82
fangyuanziti <tiziyuanfang@gmail.com>
2014-04-03 00:13:11 +0800

    add new config

--- init.el
+++ init.el
@@ -15,19 +15,11 @@ re-downloaded in order to locate PACKAGE."
         (package-refresh-contents)
         (require-package package min-version t)))))
 
+
 (package-initialize)
 
-;;; evil configs
-(require-package 'evil)
-
-(setq evil-search-module 'evil-search
-      evil-want-C-u-scroll t
-      evil-want-C-w-in-emacs-state t)
-
-(setq evil-auto-indent t)
-
-(require 'evil)
-(evil-mode t)
+;; Set up load path
+(add-to-list 'load-path user-emacs-directory)
 
 (require-package 'undo-tree)
 (require 'undo-tree)
@@ -36,14 +28,24 @@ re-downloaded in order to locate PACKAGE."
 (require-package 'solarized-theme)
 (require 'solarized-dark-theme)
 
+;; Turn off mouse interface early in startup to avoid momentary display
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 ;;; hide the splash-screen
 (setq inhibit-splash-screen t)
 
 
 ;;;General config
+
 (setq scroll-margin 3)
+;; Write backup files to own directory
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name
+                 (concat user-emacs-directory "backups")))))
+;; Make backups of files, even when they're in version control
+(setq vc-make-backup-files t)
 
-
+;;; Multi-term config
 (require-package 'multi-term)
 (require 'multi-term)
 (setq multi-term-program "/bin/bash")
@@ -59,3 +61,86 @@ re-downloaded in order to locate PACKAGE."
 (add-hook 'term-mode-hook
           (lambda ()
             (define-key term-raw-map (kbd "C-y") 'term-paste)))
+
+;;;
+(require-package 'evil-leader)
+(require 'evil-leader)
+(global-evil-leader-mode)
+;;; evil configs
+(require-package 'evil)
+
+(setq evil-search-module 'evil-search
+      evil-want-C-u-scroll t
+      evil-want-C-w-in-emacs-state t)
+
+(setq evil-auto-indent t)
+
+(require 'evil)
+(evil-mode t)
+
+;;; esc quits for vim mode
+(define-key evil-normal-state-map [escape] 'keyboard-quit)
+(define-key evil-visual-state-map [escape] 'keyboard-quit)
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+;; (define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+;; (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+
+(define-key evil-normal-state-map "\C-h" 'evil-window-left)
+(define-key evil-normal-state-map "\C-j" 'evil-window-down)
+(define-key evil-normal-state-map "\C-k" 'evil-window-up)
+(define-key evil-normal-state-map "\C-l" 'evil-window-right)
+(define-key evil-normal-state-map "\C-l" 'evil-window-right)
+
+(define-key evil-normal-state-map "\S-H" 'evil-beginning-of-line)
+
+(define-key evil-normal-state-map "\S-L" 'evil-end-of-line)
+
+
+
+;; (define-key evil-normal"s" 'evil-window-split)
+;;; get basic emacs commmands in insert mode (has bug)
+;; (define-key evil-normal-state-map "\C-y" 'yank)
+;; (define-key evil-insert-state-map "\C-y" 'yank)
+;; (define-key evil-visual-state-map "\C-y" 'yank)
+;; (define-key evil-insert-state-map "\C-e" 'end-of-line)
+;; (define-key evil-normal-state-map "\C-w" 'evil-delete)
+;; (define-key evil-insert-state-map "\C-w" 'evil-delete)
+;; (define-key evil-insert-state-map "\C-r" 'search-backward)
+;; (define-key evil-visual-state-map "\C-w" 'evil-delete)
+
+;;; keep special mode when in those buffer
+(loop for (mode . state) in '((inferior-emacs-lisp-mode . emacs)
+                              (nrepl-mode . insert)
+                              (pylookup-mode . emacs)
+                              (comint-mode . normal)
+                              (shell-mode . insert)
+                              (git-commit-mode . insert)
+                              (git-rebase-mode . emacs)
+                              (term-mode . emacs)
+                              (help-mode . emacs)
+                              (helm-grep-mode . emacs)
+                              (grep-mode . emacs)
+                              (bc-menu-mode . emacs)
+                              (magit-branch-manager-mode . emacs)
+                              (rdictcc-buffer-mode . emacs)
+                              (dired-mode . emacs)
+                              (Wdired-mode . normal))
+      do (evil-set-initial-state mode state))
+
+
+;; leader key config
+(evil-leader/set-leader ",")
+(evil-leader/set-key
+  "e" 'find-file
+  "b" 'switch-to-buffer
+  "k" 'kill-buffer
+  "s" 'evil-window-split
+  "v" 'evil-window-vsplit
+  "w" 'evil-write)
+
+
+(require-package 'company)
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
